8f240bbaedf1fb04175a1c73796ce54e4c9a609b
code-chip <willvix@outlook.com>
2022-01-31 11:41:41 -0300

    07 Removendo swithc-case com Strategy.

--- design-patterns/padroes-comportamentais/testes.php
+++ design-patterns/padroes-comportamentais/testes.php
@@ -4,10 +4,12 @@ require 'vendor/autoload.php';
 
 use Alura\DesignPattern\CalculadoraDeImpostos;
 use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\Impostos\Icms;
+use Alura\DesignPattern\Impostos\Iss;
 
 $calculadora = new CalculadoraDeImpostos();
 
 $orcamento = new Orcamento();
 $orcamento->valor = 100;
 
-echo $calculadora->calcula($orcamento, "ISS");
+echo $calculadora->calcula($orcamento, new Icms());
