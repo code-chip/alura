0beb92eb860bc79ed1a7f51610853830dd00a4d2
code-chip <willvix@outlook.com>
2022-02-10 12:01:24 -0300

    06 Observer - Ações ao gerar um pedido.

--- design-patterns/padroes-comportamentais/src/GerarPedidoHandler.php
+++ design-patterns/padroes-comportamentais/src/GerarPedidoHandler.php
@@ -2,6 +2,9 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\AcoesAoGerarPedido\CriarPedidoNoBanco;
+use Alura\DesignPattern\AcoesAoGerarPedido\EnviarPedidoPorEmail;
+use Alura\DesignPattern\AcoesAoGerarPedido\LogGerarPedido;
 use Alura\DesignPattern\GerarPedido;
 
 class GerarPedidoHandler
@@ -22,10 +25,12 @@ class GerarPedidoHandler
         $pedido->nomeCliente = $gerarPedido->getNomeCliente();
         $pedido->orcamento = $orcamento;
 
-        // PedidosRepository
-        echo "Cria pedido no banco de dados " . PHP_EOL;
-        // MailService
-        echo "Envia e-mail para cliente " . PHP_EOL;
-        //
+        $pedidosRepository = new CriarPedidoNoBanco();
+        $logGerarPedido = new LogGerarPedido();
+        $enviarPedidoPorEmail = new EnviarPedidoPorEmail();
+
+        $pedidosRepository->executaAcao($pedido);
+        $logGerarPedido->executaAcao($pedido);
+        $enviarPedidoPorEmail->executaAcao($pedido);
     }
 }
