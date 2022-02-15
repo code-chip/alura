68290c157db729d45726102ba1a2ffc1826135ee
code-chip <willvix@outlook.com>
2022-02-14 23:31:22 -0300

    01 Por que testar - Testes automatizados.

--- PHP-e-TDD-tetes-com-PHPUnit/teste-avaliador.php
+++ PHP-e-TDD-tetes-com-PHPUnit/teste-avaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Model\Usuario;
 use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Service\Avaliador;
 
+//Arrumo a casa para o teste (Arrange - Give)
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -16,8 +17,19 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+//Executa o código a ser testado (Act - When)
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
+
+//Verifico se a saída é a esperada (Assert - Then)
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+    echo "TESTE OK" . PHP_EOL;
+} else {
+    echo "TESTE FALHOU" . PHP_EOL;
+}
 
 echo $maiorValor . PHP_EOL;
