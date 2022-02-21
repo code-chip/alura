f35154150a9984033b493f966162a091ebf5d269
code-chip <willvix@outlook.com>
2022-02-20 23:11:46 -0300

    04 Organizando os testes - Métodos setUp e tearDown.

--- PHP-e-TDD-testes-com-PHPUnit/tests/AvaliadorTest.php
+++ PHP-e-TDD-testes-com-PHPUnit/tests/AvaliadorTest.php
@@ -10,6 +10,12 @@ use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase
 {
+    private $leiloeiro;
+
+    public function setUp(): void
+    {
+        $this->leiloeiro = new Avaliador();
+    }
     /**
      * @dataProvider leilaoEmOrdemAleatoria
      * @dataProvider leilaoEmOrdemCrescente
@@ -17,12 +23,10 @@ class AvaliadorTest extends TestCase
      */
     public function testAvaliadorDeveEncontrarOMaiorValorDeLances(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
-
         //Executa o código a ser testado (Act - When)
-        $leiloeiro->avalia($leilao);
+        $this->leiloeiro->avalia($leilao);
 
-        $maiorValor = $leiloeiro->getMaiorValor();
+        $maiorValor = $this->leiloeiro->getMaiorValor();
 
         //Verifico se a saída é a esperada (Assert - Then)
         self::assertEquals(2500, $maiorValor);
@@ -37,12 +41,10 @@ class AvaliadorTest extends TestCase
      */
     public function testAvaliadorDeveEncontrarOMenorValorDeLances(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
-
         //Executa o código a ser testado (Act - When)
-        $leiloeiro->avalia($leilao);
+        $this->leiloeiro->avalia($leilao);
 
-        $menorValor = $leiloeiro->getMenorValor();
+        $menorValor = $this->leiloeiro->getMenorValor();
 
         //Verifico se a saída é a esperada (Assert - Then)
         self::assertEquals(1700, $menorValor);
@@ -57,16 +59,16 @@ class AvaliadorTest extends TestCase
      */
     public function testAvaliadorDeveBuscar3MaioresLances(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
-        $leiloeiro->avalia($leilao);
+        $this->leiloeiro->avalia($leilao);
 
-        $maiores = $leiloeiro->getMaioresLances();
+        $maiores = $this->leiloeiro->getMaioresLances();
         static::assertCount(3, $maiores);
         static::assertEquals(2500, $maiores[0]->getValor());
         static::assertEquals(2000, $maiores[1]->getValor());
         static::assertEquals(1700, $maiores[2]->getValor());
     }
 
+    /* --- DADOS --- */
     public function leilaoEmOrdemCrescente(): array
     {
         $leilao = new Leilao('Fiat 147 0KM');
