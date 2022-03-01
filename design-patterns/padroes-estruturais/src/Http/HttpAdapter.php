d5331ed480756954fecf707aa27dbd3ec4ea1f88
code-chip <willvix@outlook.com>
2022-03-01 20:20:12 -0300

    Design Patterns em PHP - Padrões estruturais - Criando um adapter.

---
+++ design-patterns/padroes-estruturais/src/Http/HttpAdapter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern\Http;
+
+class HttpAdapter
+{
+    public function post(string $url, array $data = []): void
+    {
+
+    }
+}
--- design-patterns/padroes-estruturais/src/RegistroOrcamento.php
+++ design-patterns/padroes-estruturais/src/RegistroOrcamento.php
@@ -2,10 +2,25 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\EstadosOrcamento\Finalizado;
+use HttpAdapter;
+
 class RegistroOrcamento
 {
+    private HttpAdapter $http;
+
+    public function __construct(HttpAdapter $http)
+    {
+        $this->http = $http;
+    }
     public function registrar(Orcamento $orcamento): void
     {
-
+        if (!$orcamento->estadoAtual instanceof Finalizado) {
+            throw new \DomainException('Apenas orçamentos finalizados podem ser registrados na API.')
+        }
+        $this->http->post('http://api.registrar.orcamento', [
+            'valor' => $orcamento->valor,
+            'quantidadeItens' => $orcamento->quantidadeItens
+        ]);
     }
 }
