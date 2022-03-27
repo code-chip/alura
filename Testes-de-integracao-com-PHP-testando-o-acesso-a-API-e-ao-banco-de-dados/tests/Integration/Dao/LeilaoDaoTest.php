8515f4ce76820c798bf729a3ca6df3366b48ccb7
code-chip <willvix@outlook.com>
2022-03-27 13:55:35 -0300

    02 Garantindo a integridade - Trabalhando com transações.

--- Testes-de-integracao-com-PHP-testando-o-acesso-a-API-e-ao-banco-de-dados/tests/Integration/Dao/LeilaoDaoTest.php
+++ Testes-de-integracao-com-PHP-testando-o-acesso-a-API-e-ao-banco-de-dados/tests/Integration/Dao/LeilaoDaoTest.php
@@ -5,16 +5,23 @@ namespace Alura\Leilao\Tests\Integration\Dao;
 use Alura\Leilao\Dao\Leilao as LeilaoDao;
 use Alura\Leilao\Infra\ConnectionCreator;
 use Alura\Leilao\Model\Leilao;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+    private PDO $pdo;
+
+    public function setUp(): void
+    {
+        $this->pdo = ConnectionCreator::getConnection();
+        $this->pdo->beginTransaction();
+    }
     public function testInsercaoEBuscaDevemFuncionar()
     {
         // arrange
         $leilao = new Leilao('Variante 0Km');
-        $pdo = ConnectionCreator::getConnection();
-        $leilaoDao = new LeilaoDao($pdo);
+        $leilaoDao = new LeilaoDao($this->pdo);
         $leilaoDao->salva($leilao);
 
         // act
@@ -25,7 +32,11 @@ class LeilaoDaoTest extends TestCase
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame('Variante 0Km', $leiloes[0]->recuperarDescricao());
 
-        //tear down
-        $pdo->exec('DELETE FROM leiloes;');
+
+    }
+
+    public function tearDown(): void
+    {
+        $this->pdo->rollBack();
     }
 }
